c58e119b98fe487e8b39730f676c432d8c78158a
Max Hallgren <144932366+MaxHGit@users.noreply.github.com>
2024-02-09 19:39:35 +0100

    Rensade lite i mapparna och tog bort default Vue layouten

---
+++ src/components/AboutContent.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="main-container">
+    <h1 class="text-container">This is the about page</h1>
+  </div>
+</template>
+
+<script setup></script>
+
+<style scoped></style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,11 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+import HomeContent from "../components/HomeContent.vue";
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <HomeContent />
   </main>
 </template>
+
+<style></style>
